7eeb647c9254c8ea77078c2395596d31fb27c0ab
AH Uyekita <anderson.uyekita@gmail.com>
2019-02-11 04:17:47 -0200

    Project 04 - Done - ND111

---
+++ 05-Chapter05/00-Project_04/poi_id.py
@@ -0,0 +1,280 @@
+#!/usr/bin/python
+
+################################################################################
+#                                                                              #
+#   Project 04 - Identify Fraud from Enron Email                               #
+#                                                                              #
+#   Author: Anderson Hitoshi Uyekita                                           #
+#   Course: Data Science - Foundations II                                      #
+#   COD: ND111                                                                 #
+#   Date: 11/02/2019                                                           #
+#   Version: 1.0                                                               #
+#                                                                              #
+################################################################################
+
+import sys
+import pickle
+sys.path.append("../tools/")
+
+from feature_format import featureFormat, targetFeatureSplit
+from tester import dump_classifier_and_data
+import pandas as pd
+import numpy as np
+import tester
+
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+# You will need to use more features
+
+features_list =  ['poi',
+ 'exercised_stock_options',
+ 'deferred_income',
+ 'loan_advances',
+ 'ratio_to_from',
+ 'ratio_to_poi']
+
+
+### Load the dictionary containing the dataset
+with open("final_project_dataset.pkl", "r") as data_file:
+    data_dict = pickle.load(data_file)
+
+# Converting dictionary in dataframe.
+df_dict = pd.DataFrame(data_dict).transpose()
+
+# Copying the original dictionary.
+data_dict_clean = data_dict.copy()
+
+### Task 2: Remove outliers
+
+data_dict_clean.pop('TOTAL');
+data_dict_clean.pop('LOCKHART EUGENE E');
+data_dict_clean.pop('THE TRAVEL AGENCY IN THE PARK');
+
+# Temporaly variable only to justify this test.
+df_clean = pd.DataFrame(data_dict_clean).transpose()
+
+# Copying the df_clean, because I need a backup.
+df_eda = df_clean.copy()
+
+def str_nan_to_0(df, variable):
+    """
+    +--------------------------------------------------------------------------------------------------+
+    |DESCRIPTION:                                                                                      |
+    |                                                                                                  |
+    |    This function converts the string NaN to zero.                                                |
+    |                                                                                                  |
+    +--------------------------------------------------------------------------------------------------+
+    |INPUT:                                                                                            |
+    |                                                                                                  |
+    |    VARIABLE:                                                                                     |
+    |                                                                                                  |
+    |    df: Dataframe to perform the conversion.                                                      |
+    |                                                                                                  |
+    |    variable: The columns name to be converted.                                                   |
+    |                                                                                                  |
+    +--------------------------------------------------------------------------------------------------+
+    |OUTPUT:                                                                                           |
+    |                                                                                                  |
+    |    VARIABLE:                                                                                     |
+    |                                                                                                  |
+    |    df: Dataframe with the converted columns.                                                     |
+    |                                                                                                  |
+    +--------------------------------------------------------------------------------------------------+
+    """
+    # Temporaly list
+    temp = []
+
+    # When the NaN is a string.
+    # Loop to fix the NaN values.
+    for index in df[variable]:
+        if index == 'NaN':
+            temp.append(0)
+        else:
+            temp.append(float(index))
+
+    # Updatind the columns with NaN with list with 0.
+    df[variable] = temp
+
+    return df
+
+# Loop to convert all columns from df_eda.
+for index in df_eda:
+    # These two features do not need this conversion.
+    if index in ['email_address']:
+        continue
+    else:
+        # Convert String NaN to 0.
+        str_nan_to_0(df = df_eda, variable = index);
+
+        # Convert float NaN to 0.
+        df_eda[index] = df_eda[index].fillna(0)
+
+### Task 3: Create new feature(s)
+
+# Defining a function to create the feature engineering.
+def feature_engineering(df, ratio_name, variable_numerator, variable_denominator):
+    """
+    +--------------------------------------------------------------------------------------------------+
+    |DESCRIPTION:                                                                                      |
+    |                                                                                                  |
+    |    This function creates the ratio between two variables and store to a dataframe.               |
+    |                                                                                                  |
+    +--------------------------------------------------------------------------------------------------+
+    |INPUT:                                                                                            |
+    |                                                                                                  |
+    |    VARIABLE:                                                                                     |
+    |                                                                                                  |
+    |    df: Dataframe to insert the new ratio.                                                        |
+    |                                                                                                  |
+    |    ratio_name: Ratio's name.                                                                     |
+    |                                                                                                  |
+    |    variable_numerator: The feature to be divided by the numerator.                               |
+    |                                                                                                  |
+    |    variable_denominator: The feature to divide the denominator.                                  |
+    |                                                                                                  |
+    +--------------------------------------------------------------------------------------------------+
+    |OUTPUT:                                                                                           |
+    |                                                                                                  |
+    |    VARIABLE:                                                                                     |
+    |                                                                                                  |
+    |    df: Dataframe with the ratio stored as a new column.                                          |
+    |                                                                                                  |
+    +--------------------------------------------------------------------------------------------------+
+    """
+    # Temporaly variable.
+    temp = []
+
+    # Loop to create the ratio.
+    for num,den in zip(variable_numerator, variable_denominator):
+        # For each value will analise if is a NaN or not.
+        if den == 0:
+            temp.append(0.)
+        elif (num != 'NaN') & (den != 'NaN'):
+            temp.append(num/float(den))
+        else:
+            temp.append(0.)
+
+    # Adding the new column.
+    df[str(ratio_name)] = temp
+
+    return df
+
+# 1. Adding the ratio_from_poi.
+feature_engineering(df = df_eda,
+                    ratio_name = 'ratio_from_poi',
+                    variable_numerator = df_eda.from_poi_to_this_person,
+                    variable_denominator = df_eda.from_messages);
+
+# 2. Adding the ratio_to_poi.
+feature_engineering(df = df_eda,
+                    ratio_name = 'ratio_to_poi',
+                    variable_numerator = df_eda.from_this_person_to_poi,
+                    variable_denominator = df_eda.to_messages);
+
+# 3. Adding the ratio_to_from.
+feature_engineering(df = df_eda,
+                    ratio_name = 'ratio_to_from',
+                    variable_numerator = df_eda.to_messages,
+                    variable_denominator = df_eda.from_messages);
+
+# 4. Adding the ratio_from_poi_share.
+feature_engineering(df = df_eda,
+                    ratio_name = 'ratio_from_poi_share',
+                    variable_numerator = df_eda.shared_receipt_with_poi,
+                    variable_denominator = df_eda.from_messages);
+
+# 5. Adding the ratio_salary_total_payments.
+feature_engineering(df = df_eda,
+                    ratio_name = 'ratio_salary_total_payments',
+                    variable_numerator = df_eda.salary,
+                    variable_denominator = df_eda.total_payments);
+
+# Saving the dataset in dictionary type.
+my_dataset = df_eda.to_dict(orient = 'index')
+
+### Store to my_dataset for easy export below.
+#my_dataset = data_dict
+
+### Extract features and labels from dataset for local testing
+#data = featureFormat(my_dataset, features_list, sort_keys = True)
+#labels, features = targetFeatureSplit(data)
+
+# Following the instructions.
+#my_dataset = dict_eda
+
+# This is
+def wrapper_featureformatsplit(my_dataset, features_list):
+    """
+    +--------------------------------------------------------------------------------------------------+
+    |DESCRIPTION:                                                                                      |
+    |                                                                                                  |
+    |    This function runs two scripts: featureFormat and targetFeatureSplit.                         |
+    |                                                                                                  |
+    +--------------------------------------------------------------------------------------------------+
+    |INPUT:                                                                                            |
+    |                                                                                                  |
+    |    VARIABLE:                                                                                     |
+    |                                                                                                  |
+    |    my_dataset: This is a dictionary.                                                             |
+    |                                                                                                  |
+    |    features_list: List of variable to be gathered from my_dataset.                               |
+    |                                                                                                  |
+    +--------------------------------------------------------------------------------------------------+
+    |OUTPUT:                                                                                           |
+    |                                                                                                  |
+    |    VARIABLE:                                                                                     |
+    |                                                                                                  |
+    |    labels: This is the POI feature from my_dataset.                                              |
+    |                                                                                                  |
+    |    features: This is an array with the features_list values.                                     |
+    |                                                                                                  |
+    +--------------------------------------------------------------------------------------------------+
+    """
+    # Creating data ACCORDING to the features_list, which is input of targetFeatureSplit.
+    data = featureFormat(my_dataset, features_list, sort_keys = True)
+
+    # Using data to create labels and features.
+    labels, features = targetFeatureSplit(data)
+
+    # Returning the labels and features.
+    return labels, features
+
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+# Please, find more information in the Jupyter Notebook.
+
+# Importing the AdaBoost from Scikit learn package.
+from sklearn.ensemble import AdaBoostClassifier
+
+# Creating a classifier with the optimized parameters.
+clf_ada = AdaBoostClassifier(learning_rate = 1.0, n_estimators = 60)
+
+# Testing the classifier.
+tester.test_classifier(clf = clf_ada,
+                       dataset = my_dataset,
+                       feature_list = features_list)
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info:
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+# Please, find more information in the Jupyter Notebook.
+
+# Example starting point. Try investigating other evaluation techniques!
+
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
+
+dump_classifier_and_data(clf = clf_ada, dataset = my_dataset, feature_list = features_list)
